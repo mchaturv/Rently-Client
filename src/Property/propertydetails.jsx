72f9f9aa26a29e1665242afa84f543e8b8091dee
Syed Abrar <sy780925@dal.ca>
2020-08-04 20:15:59 -0300

    Testing Chat v.0.2 for demo... Not final

--- src/Property/propertydetails.jsx
+++ src/Property/propertydetails.jsx
@@ -299,7 +299,7 @@ class PropertyDetails extends React.Component {
                       Contact the Owner
                     </a>
 
-                    <a href="#" class="makeappointment-btn">
+                    <a href="http://localhost:3000/" class="makeappointment-btn">
                       <BookingComponent>Make an Appointment</BookingComponent>
                     </a>
                   </div>
