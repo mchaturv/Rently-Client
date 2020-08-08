3cf68f548cc135814e1785014182a8344cdd3adb
Syed Abrar <sy780925@dal.ca>
2020-08-08 00:09:46 -0300

    Update propertydetails.jsx

--- src/Property/propertydetails.jsx
+++ src/Property/propertydetails.jsx
@@ -295,7 +295,7 @@ class PropertyDetails extends React.Component {
                       sed do eiusmod tempor incididunt ut labore et dolore magna
                       aliqua.{" "}
                     </p>
-                    <a href="https://rently-messaging-interface.herokuapp.com/" class="contactowner-btn">
+                    <a href="https://rently-gruop13.herokuapp.com/" class="contactowner-btn">
                       Contact the Owner
                     </a>
 
